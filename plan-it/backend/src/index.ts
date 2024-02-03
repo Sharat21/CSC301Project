bd632750a6bc95e218290e09ebb3acbf5dc3a538
Waleed Haddad <waleed.haddad@mail.utoronto.com>
2024-02-02 20:11:36 -0500

    Connection to mongoDB can now be made through index.ts

--- plan-it/backend/src/index.ts
+++ plan-it/backend/src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello World");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://waleed:test-123@cluster0.1z7sdxd.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const db = client.db("plan-it");
+
+        const col = db.collection("users");
+
+        const filter = {"Firstname": "Waleed"};
+        const document = await col.findOne(filter);
+        console.log("Document found:\n" + JSON.stringify(document));
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch(err) {
+        console.log(err.stack);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
