7148b64b0a5d9c2026eca96b44c12ac4cad6af98
Waleed Haddad <waleed.haddad@mail.utoronto.com>
2024-02-02 21:20:57 -0500

    database CRUD functions setup, minimal testing completed

--- plan-it/backend/src/index.ts
+++ plan-it/backend/src/index.ts
@@ -1,35 +1,46 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://waleed:test-123@cluster0.1z7sdxd.mongodb.net/?retryWrites=true&w=majority";
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://waleed:test-123@cluster0.1z7sdxd.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 
-async function run() {
-    try {
-        // Connect the client to the server
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        const db = client.db("plan-it");
+// async function run() {
+//     try {
+//         // Connect the client to the server
+//         await client.connect();
+//         // Send a ping to confirm a successful connection
+//         const db = client.db("plan-it");
 
-        const col = db.collection("users");
+//         const col = db.collection("users");
 
-        const filter = {"Firstname": "Waleed"};
-        const document = await col.findOne(filter);
-        console.log("Document found:\n" + JSON.stringify(document));
+//         const filter = {"Firstname": "Waleed"};
+//         const document = await col.findOne(filter);
+//         console.log("Document found:\n" + JSON.stringify(document));
 
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch(err) {
-        console.log(err.stack);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     } catch(err) {
+//         console.log(err.stack);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
 
-run().catch(console.dir);
+// run().catch(console.dir);
+
+const database = require('./database');
+
+createExample();
+
+// Example usage of createDocument
+async function createExample() {
+    const data = { Firstname: "Lebron", Lastname: "James", Groups: [], Trips: [], Email: "pookiebear@gmail.com", password: "glorious_king" };
+    const result = await database.addUser(data);
+    console.log("New document created:", result);
+  }
