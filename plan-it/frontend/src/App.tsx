e81017c62537fb3c3aa2a695314b0b505c0af7f6
Ryan <ryanr.4849@gmail.com>
2024-02-11 18:25:09 -0500

    Merging TripDetails into main

--- plan-it/frontend/src/App.tsx
+++ plan-it/frontend/src/App.tsx
@@ -5,17 +5,17 @@ import React from "react";
   // return <p>Hello World</p>;
   //return <div ><Trips /></div>
   //return <div> <Ideas /> </div>
-import LoginPage from "./components/LoginPage";
-import Bar  from "./TripDetails/Bar";
-import TripDetails from "./TripDetails/TripDetails";
-import Restaurants from "./TripDetails/pages/Restaurants"
-import BudgetSheet from "./TripDetails/BudgetSheet";
+// import LoginPage from "./components/LoginPage";
+// import Bar  from "./TripDetails/Bar";
+// import TripDetails from "./TripDetails/TripDetails";
+// import Restaurants from "./TripDetails/pages/Restaurants"
+// import BudgetSheet from "./TripDetails/BudgetSheet";
 // import BudgetSheet from "./TripDetails/pages/BudgetSheet";
 
 function App() {
   return (
     <div>
-      <TripDetails />
+      {/* <TripDetails /> */}
     </div>
   );
 }
