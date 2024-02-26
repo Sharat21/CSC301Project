5a9076f3c19bb7480f8b8a356ecb8238a68ab36c
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2024-02-26 12:15:19 -0500

    created new form/dialog and updated idea + idea list with new idea information

---
+++ plan-it/frontend/src/Ideas/components/AddIdeaDialog.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    MenuItem,
+    Select,
+    TextField
+} from '@mui/material';
+
+const AddIdeaDialog = ({ open, handleClose, newIdea, setNewIdea, handleSubmit }) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewIdea({ ...newIdea, [name]: value });
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add a New Idea</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="Name"
+                    label="Name of the Idea"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    value={newIdea.Name}
+                    onChange={handleChange}
+                />
+                <Select
+                    margin="dense"
+                    name="Type"
+                    label="Type of Idea"
+                    fullWidth
+                    displayEmpty
+                    value={newIdea.Type}
+                    onChange={handleChange}
+                    variant="outlined"
+                >
+                    <MenuItem value="" disabled>
+                        <em>Type of Idea</em>
+                    </MenuItem>
+                    {['Destination', 'Transportation', 'Activity', 'Restaurant', 'Accommodation'].map((type) => (
+                        <MenuItem key={type} value={type}>{type}</MenuItem>
+                    ))}
+                </Select>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    name="Description"
+                    label="Description of the Idea"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    value={newIdea.Description}
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="dense"
+                    name="link"
+                    label="Link for the Idea (Optional)"
+                    type="url"
+                    fullWidth
+                    variant="outlined"
+                    value={newIdea.link}
+                    onChange={handleChange}
+                />
+                <TextField
+                    margin="dense"
+                    name="price"
+                    label="Expense/Price of Idea (Optional)"
+                    type="text"
+                    fullWidth
+                    variant="outlined"
+                    value={newIdea.price}
+                    onChange={handleChange}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Close</Button>
+                <Button onClick={handleSubmit} color='success'>Submit</Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default AddIdeaDialog;
--- plan-it/frontend/src/Ideas/components/IdeaList.jsx
+++ plan-it/frontend/src/Ideas/components/IdeaList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Idea from './Idea';
 import { Box, Grid } from '@mui/material';
 
@@ -8,7 +8,13 @@ const IdeaList = ({ ideas }) => {
       <Grid container spacing={2}>
         {ideas.map((idea) => (
           <Grid item key={idea.id} xs={12} sm={6} md={4} lg={3}>
-            <Idea content={idea.content} date={idea.date} />
+            <Idea 
+              name={idea.Name} 
+              type={idea.Type} 
+              description={idea.Description}
+              link={idea.link}
+              price={idea.price}
+            />
           </Grid>
         ))}
       </Grid>
